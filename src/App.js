4bcd9e793bfff256e7e580fc8bfb254e70b615f7
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-11 23:19:16 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+
+// page components
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Article from './pages/Article'
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+          <NavLink exact to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
 
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/articles/:id" element={<Article />} />
+        </Routes>
+
+      </BrowserRouter>
+    </div >
   );
 }
 
-export default App;
+export default App
